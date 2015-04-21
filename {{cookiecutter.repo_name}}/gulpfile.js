5c916a63c2af423c3bec697807bd4e87134b8d11
Dries Desmet <dries@urga.be>
2015-04-21 19:15:05 +0200

    Add browser-sync

--- {{cookiecutter.repo_name}}/gulpfile.js
+++ {{cookiecutter.repo_name}}/gulpfile.js
@@ -1,39 +1,59 @@
+'use strict';
+
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var watch = require('gulp-watch');
-var minifycss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var gzip = require('gulp-gzip');
-var livereload = require('gulp-livereload');
+var $ = require('gulp-load-plugins')();
+var childProcess = require('child_process');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
-var gzip_options = {
-    threshold: '1kb',
-    gzipOptions: {
-        level: 9
+// Compile  stylesheets
+gulp.task('styles', function () {
+  // For best performance, don't add Sass partials to `gulp.src`
+  return gulp.src([
+    '{{ cookiecutter.repo_name }}/static/scss/main.scss'
+  ])
+    .pipe($.sourcemaps.init())
+    //.pipe($.changed('{{ cookiecutter.repo_name }}/static/css', {extension: '.css'}))
+    .pipe($.sass({
+      precision: 10,
+      onError: console.error.bind(console, 'Sass error:')
+    }))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('{{ cookiecutter.repo_name }}/static/css'))
+    // Concatenate and minify styles
+    //.pipe($.if('*.css', $.csso()))
+    //.pipe(gulp.dest('{{ cookiecutter.repo_name }}/static/css/min'))
+    .pipe($.size({title: 'styles'}));
+});
+
+gulp.task('runserver', function() {
+  var runserver;
+  runserver = childProcess.exec('./manage.py runserver', function (error, stdout, stderr) {
+    if (error) {
+      console.log(error.stack);
+      console.log('Error code: ' + error.code);
+      console.log('Signal received: ' + error.signal);
     }
-};
+    console.log('runserver STDOUT: ' + stdout);
+    console.log('runserver STDERR: ' + stderr);
+  });
 
-/* Compile Our Sass */
-gulp.task('sass', function() {
-    return gulp.src('{{ cookiecutter.repo_name }}/static/scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('{{ cookiecutter.repo_name }}/static/css'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(minifycss())
-        .pipe(gulp.dest('{{ cookiecutter.repo_name }}/static/css'))
-        .pipe(gzip(gzip_options))
-        .pipe(gulp.dest('{{ cookiecutter.repo_name }}/static/css'))
-        .pipe(livereload());
+  runserver.on('exit', function (code) {
+    console.log('Child process exited with exit code '+code);
+  });
 });
 
-/* Watch Files For Changes */
-gulp.task('watch', function() {
-    livereload.listen();
-    gulp.watch('{{ cookiecutter.repo_name }}/static/scss/*.scss', ['sass']);
+// Watch files for changes & reload
+gulp.task('serve', ['styles', 'runserver'], function () {
+  browserSync({
+    notify: false,
+    // Customize the BrowserSync console logging prefix
+    logPrefix: 'EGICO',
+    open: false,
+    proxy: "127.0.0.1:8000"
+  });
 
-    /* Trigger a live reload on any Django template changes */
-    gulp.watch('**/templates/*').on('change', livereload.changed);
-
+  //gulp.watch(['app/**/*.html'], reload);
+  gulp.watch('*/templates/**/*.html', [reload]);
+  gulp.watch(['{{ cookiecutter.repo_name }}/static/scss/**/*.{scss,css}'], ['styles', reload]);
 });
-
-gulp.task('default', ['sass', 'watch']);
